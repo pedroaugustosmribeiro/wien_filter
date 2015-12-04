78190b462c192589dfd7773b55bb09f30715e34f
Pedro Augusto Sampaio Messias Ribeiro <pedro.augusto.sampaio.messias@gmail.com>
2015-12-05 00:19:02 +0200

    Mudanças a serem submetidas: 	new file:   src/build.sh

---
+++ src/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+fcomp=$1
+fopt=``
+    if [ "$fcomp" == 'ifort' ]; then
+	fopt='-fast -CB'
+    elif [ "$fcomp" == 'gfortran' ]; then
+	fopt='-fbounds-check'
+    else
+	echo './build.sh [ifort or gfortran]'
+	exit 1
+    fi	
+$fcomp -c $fopt mod/values.f90
+$fcomp -c $fopt mod/*.f90
+$fcomp  $fopt main.f90 mod/*.f90 -o wien_filter
+rm *.o
+rm *.mod
