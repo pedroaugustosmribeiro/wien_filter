866f26b26c1bfcb3ea70681b4cdd4eb42310be70
Pedro Augusto Sampaio Messias Ribeiro <pedro.augusto.sampaio.messias@gmail.com>
2015-12-11 20:48:48 +0200

    Mudanças a serem submetidas: 	modified:   build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 fcomp=$1
+fmode=$2
 fopt=``
+fdevel=``
     if [ "$fcomp" == 'ifort' ]; then
-	fopt='-fast -CB -parallel -ipo'
+	fopt='-fast -parallel -qopenmp -fno-alias -m64 -simd -qoffload-arch=ivybridge'
+	if [ "$fmode" == 'devel' ]; then
+	    fdevel='-CB'	    
+	fi
     elif [ "$fcomp" == 'gfortran' ]; then
-	fopt='-fbounds-check'
+	fopt='-O3'
+	if [ "$fmode" == 'devel' ]; then
+	    fdevel='-fbounds-check'	    
+	fi
     else
 	echo './build.sh [ifort or gfortran]'
 	exit 1
     fi	
-$fcomp -c $fopt mod/values.f90
-$fcomp -c $fopt mod/*.f90
-$fcomp  $fopt main.f90 mod/*.f90 -o wien_filter
+$fcomp -c $fopt $fdevel mod/values.f90
+$fcomp -c $fopt $fdevel mod/*.f90
+$fcomp  $fopt $fdevel main.f90 mod/*.f90 -o wien_filter
 rm *.o
 rm *.mod
